0651ea546b82b420688b3e48f6b790cd764bb216
NickSwitchSchool <97087180@st.deltion.nl>
2022-03-14 15:14:58 +0100

    nieuwe scene gemaakt
    heb een nieuwe scene gemaakt op een duidelijkere plek om unity netjes te houden

--- Shadow Heart/Assets/Scripts/GolemAI.cs
+++ Shadow Heart/Assets/Scripts/GolemAI.cs
@@ -7,19 +7,37 @@ public class GolemAI : MonoBehaviour
 {
     public Transform goal;
 
+    public Vector3 pos;
+    public Vector3 targetDirection;
+    public Vector3 direction;
+
+    public RaycastHit inRange;
+
     public float speedGolem;
+    public float rotationSpeed;
+    public float attackRange;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.speed = speedGolem;
     }
 
     // Update is called once per frame
     void Update()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.speed = speedGolem;
-        agent.destination = goal.position;
+        targetDirection = goal.position - transform.position;
+        direction = Vector3.RotateTowards(transform.forward, targetDirection, rotationSpeed * Time.deltaTime, 0.0f);
+        transform.rotation = Quaternion.LookRotation(direction);
+        pos = transform.position;
+        if (Physics.Raycast(transform.position, transform.forward, out inRange, attackRange))
+        {
+            if (inRange.transform.tag == "Player")
+            {
+                NavMeshAgent agent = GetComponent<NavMeshAgent>();
+                agent.destination = goal.position;
+            }
+        }
     }
 }
