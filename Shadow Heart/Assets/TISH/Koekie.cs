32a1f955bab251d0f29f78125ad5738c23500583
Nitish <69513631+Redbruh@users.noreply.github.com>
2022-03-17 12:53:05 +0100

    Move and Cam script
    In het mapje van Tish

---
+++ Shadow Heart/Assets/TISH/Koekie.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Koekie : MonoBehaviour
+{
+    private const float YMin = -60.0f;
+    private const float YMax = 60.0f;
+
+    public Transform lookAt;
+
+    public Transform Player;
+
+    public float distance;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    public float sensivity;
+
+    void LateUpdate()
+    {
+        currentX += Input.GetAxis("Mouse X") * sensivity;
+        currentY += Input.GetAxis("Mouse Y") * sensivity;
+
+        currentY = Mathf.Clamp(currentY, YMin, YMax);
+
+        Vector3 Direction = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = lookAt.position + rotation * Direction;
+
+        transform.LookAt(lookAt.position);
+    }
+}
---
+++ Shadow Heart/Assets/TISH/MoveScript.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float moveVelocity;
+    public float dodgePower;
+    public int timer;
+
+    public float stamina;
+    public bool input;
+
+    public GameObject pluur;
+    public Rigidbody playerRb;
+    public bool grounded;
+    public float jumpHeight;
+
+    public float speed;
+    public Transform cam;
+
+    public float h;
+    public float v;
+
+    public void Start()
+    {
+        stamina = 100;
+        playerRb = GetComponent<Rigidbody>();
+    }
+    public void FixedUpdate()
+    {
+        //code voor bewegen
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0, v).normalized;
+        move.x = h;
+        move.z = v;
+
+        pluur.transform.position += cam.transform.rotation * move * moveVelocity * Time.deltaTime;
+
+        Debug.Log(move);
+
+        Quaternion targetRotation = Quaternion.LookRotation(move);
+        targetRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 420 * Time.fixedDeltaTime);
+
+        playerRb.MoveRotation(targetRotation);
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * cam.GetComponent<Koekie>().sensivity * Time.fixedDeltaTime);
+
+        Quaternion CamRotation = cam.rotation;
+        CamRotation.x = 0f;
+        CamRotation.z = 0f;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, CamRotation, 0.1f);
+    }
+
+    public void Update()
+    {
+        if (input == false && stamina <= 100)
+        {
+            stamina += Time.deltaTime;
+        }
+
+        if (grounded == true && stamina >= 0)
+        {
+            if (Input.GetButtonDown("Sprint"))
+            {
+                input = true;
+                moveVelocity += 8;
+                if (moveVelocity >= 15)
+                {
+                    stamina -= Time.deltaTime;
+                }
+            }
+
+            if (Input.GetButtonUp("Sprint"))
+            {
+                input = false;
+                moveVelocity -= 8;
+            }
+        }
+
+        if (grounded == true && stamina >= 15)
+        {
+            if (Input.GetButtonDown("Jumping"))
+            {
+                input = true;
+                stamina -= 15;
+                playerRb.AddForce(Vector3.up * jumpHeight);
+                grounded = false;
+            }
+            else
+            {
+                input = false;
+            }
+        }
+
+        if (grounded == true && stamina >= 20)
+        {
+            if (Input.GetButtonDown("Dodge"))
+            {
+                input = true;
+                stamina -= 20;
+                playerRb.AddForce(transform.forward * dodgePower);
+            }
+            else
+            {
+                input = false;
+            }
+        }
+    }
+
+    public void OnCollisionEnter(Collision Ground)
+    {
+        if (Ground.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+}
