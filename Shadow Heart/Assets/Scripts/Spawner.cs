65a6f23637633de3bd75bc0c5ff1d19ac42d6842
NickSwitchSchool <97087180@st.deltion.nl>
2022-04-05 11:39:42 +0200

    game scene stuff

--- Shadow Heart/Assets/Scripts/Spawner.cs
+++ Shadow Heart/Assets/Scripts/Spawner.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public float spawnDelay;
+    public float spawnDelayBat;
+    public float spawnDelayGolem;
 
     public GameObject newBat;
+    public GameObject newGolem;
     public GameObject spawner;
 
     public Transform player;
@@ -23,16 +25,16 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        spawnDelay += Time.deltaTime;
+        spawnDelayBat += Time.deltaTime;
 
-        if (spawnDelay >= 15 - GetComponent<Difficulty>().difficultyMeter)
+        if (spawnDelayBat >= 15 - GetComponent<Difficulty>().difficultyMeter)
         {
-            spawnDelay = 0;
+            spawnDelayBat = 0;
             if (pos.x <= 200f)
             {
                 pos.x += Random.Range(0, 20);
             }
-            else if  (pos.x >= 670f)
+            else if (pos.x >= 670f)
             {
                 pos.x += Random.Range(-20, 0);
             }
@@ -55,6 +57,38 @@ public class Spawner : MonoBehaviour
             }
             GameObject bat = Instantiate(newBat, pos, Quaternion.identity);
             bat.GetComponent<BatAI>().spawner = spawner;
+
+            if (spawnDelayGolem >= 60 - GetComponent<Difficulty>().difficultyMeter && GetComponent<Difficulty>().difficultyMeter >= 4)
+            {
+                spawnDelayGolem = 0;
+                if (pos.x <= 200f)
+                {
+                    pos.x += Random.Range(0, 20);
+                }
+                else if (pos.x >= 670f)
+                {
+                    pos.x += Random.Range(-20, 0);
+                }
+                else
+                {
+                    pos.x += Random.Range(-20, 20);
+                }
+
+                if (pos.z <= 130f)
+                {
+                    pos.z += Random.Range(0, 20);
+                }
+                else if (pos.z >= 720f)
+                {
+                    pos.z += Random.Range(-20, 0);
+                }
+                else
+                {
+                    pos.z += Random.Range(-20, 20);
+                }
+                GameObject golem = Instantiate(newGolem, pos, Quaternion.identity);
+                golem.GetComponent<GolemAI>().spawner = spawner;
+            }
         }
     }
 }
