52437b6e447cda37c1edb3ea2117aca5348ef788
kivose <luukvdkamp@gmail.com>
2022-04-07 00:54:55 +0200

    EINDELIJK vuur particles gefixd holy shit

--- Shadow Heart/Assets/Scripts/enemyHPscript.cs
+++ Shadow Heart/Assets/Scripts/enemyHPscript.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class enemyHPscript : MonoBehaviour
 {
-    public float fireTick;
     public float enemyHP;
-    public bool onFire;
+    public float fireTick;
     public float timeOnFire;
+    public bool onFire;
+    public bool particlesExist;
+    public GameObject fireParticles;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        if (onFire == true && particlesExist == false)
+        {
+            GameObject clone = (GameObject)Instantiate(fireParticles, GetComponent<Transform>().position, Quaternion.identity);
+            Destroy(clone, 8.0f);
+            particlesExist = true;
+        }
+
+
         if (onFire == true)
         {
             fireTick += Time.deltaTime;
@@ -28,12 +33,15 @@ public class enemyHPscript : MonoBehaviour
                 fireTick = 0;
                 enemyHP -= 1;
             }
-            if (timeOnFire >= 7)
+            if (timeOnFire >= 8)
             {
                 onFire = false;
                 timeOnFire = 0;
                 fireTick = 0;
             }
+        }
+        else
+        {
         }
     }
 }
