aa3e443b84e64b97ba6f4c11a8241498b97743a7
kivose <luukvdkamp@gmail.com>
2022-04-06 23:28:11 +0200

    fire sword doet het attempt #1

--- Shadow Heart/Assets/Scripts/enemyHPscript.cs
+++ Shadow Heart/Assets/Scripts/enemyHPscript.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class enemyHPscript : MonoBehaviour
 {
+    public float fireTick;
     public float enemyHP;
     public bool onFire;
+    public float timeOnFire;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,22 @@ public class enemyHPscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (onFire == true)
+        {
+            fireTick += Time.deltaTime;
+            timeOnFire += Time.deltaTime;
+
+            if (fireTick >= 1)
+            {
+                fireTick = 0;
+                enemyHP -= 1;
+            }
+            if (timeOnFire >= 7)
+            {
+                onFire = false;
+                timeOnFire = 0;
+                fireTick = 0;
+            }
+        }
     }
 }
