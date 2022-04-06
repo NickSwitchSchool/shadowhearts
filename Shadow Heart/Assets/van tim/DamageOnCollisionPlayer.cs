a8c0ba95c3482369dc986998b87feaafbdaedffe
kivose <luukvdkamp@gmail.com>
2022-04-06 15:12:00 +0200

    damage on collision gefixd hoop ik

--- Shadow Heart/Assets/van tim/DamageOnCollisionPlayer.cs
+++ Shadow Heart/Assets/van tim/DamageOnCollisionPlayer.cs
@@ -7,8 +7,7 @@ public class DamageOnCollisionPlayer : MonoBehaviour
     public int damage;
 
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
