b5de8dde5c93e266ee2ebc866175740f4a510333
NickSwitchSchool <97087180@st.deltion.nl>
2022-04-07 12:50:22 +0200

    health points

--- Shadow Heart/Assets/TISH/HealtPoints.cs
+++ Shadow Heart/Assets/TISH/HealtPoints.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealtPoints : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class HealtPoints : MonoBehaviour
 
     public void Update()
     {
-        
+        if (hp <= 0)
+        {
+
+        }
     }
 }
